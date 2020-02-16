0fe2db10e880901f230e271d3c3125005b7f11a2
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-16 16:03:37 -0500

    mapper2 print

--- mapreduce-test-python/logstat3/mapper2.py
+++ mapreduce-test-python/logstat3/mapper2.py
@@ -3,8 +3,9 @@
 import re
 import sys
 
-pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+# pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
-    match = pat.search(line)
-    if match:
-        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+    print(line)
+    # match = pat.search(line)
+    # if match:
+    #     print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
