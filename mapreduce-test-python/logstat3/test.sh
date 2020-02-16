b4036c1012f525cbf693bb34a774fe9ae856473b
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-16 15:35:13 -0500

    mapper2, reducer2 keep output to see structure

--- mapreduce-test-python/logstat3/test.sh
+++ mapreduce-test-python/logstat3/test.sh
@@ -9,6 +9,11 @@
 -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py \
 -input /logstat3/input/* -output /logstat3/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/part-00000
+# /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+# -file ../../mapreduce-test-python/logstat3/mapper2.py -mapper ../../mapreduce-test-python/logstat3/mapper2.py \
+# -file ../../mapreduce-test-python/logstat3/reducer2.py -reducer ../../mapreduce-test-python/logstat3/reducer2.py \
+# -input /logstat3/output/* -output /logstat3/output2/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output2/
 ../../stop.sh
