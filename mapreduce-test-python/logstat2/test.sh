611946e85ddf6e0c1b19f8d2d76c6323af5736d8
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-19 15:59:17 -0500

    test file modified-11-logstat2 output kept6

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -14,7 +14,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /logstat2/output/part-00000 /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -cp /logstat2/output/* /logstat3/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -D mapred.reduce.tasks=0 \
 -input /logstat3/input/* -output /logstat3/output/ \
