769b78705331245bae6064e55a6d0dfecb6d9b3d
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-18 22:10:13 -0500

    test file modified-11-logstat2 output kept3

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -9,6 +9,22 @@
 -file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py \
 -input /logstat2/input/* -output /logstat2/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
+
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /logstat2/output/part-00000 /logstat3/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-D mapred.reduce.tasks=0 \
+-input /logstat3/input/* -output /logstat3/output/ \
+-file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py \
+# -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py
+/usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
 # /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
 ../../stop.sh
