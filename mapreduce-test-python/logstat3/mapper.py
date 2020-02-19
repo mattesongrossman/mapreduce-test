851fdc88cea4ab9c41bb557668aed182f2c90b3c
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-18 21:54:54 -0500

    test file modified-11-logstat2 output kept2

--- mapreduce-test-python/logstat3/mapper.py
+++ mapreduce-test-python/logstat3/mapper.py
@@ -1,10 +1,23 @@
-#!/usr/bin/python
-# --*-- coding:utf-8 --*--
-import re
+#!usr/bin/python
+
+from operator import itemgetter
 import sys
 
-pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+top_ip = {}
+
 for line in sys.stdin:
-    match = pat.search(line)
-    if match:
-        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+	line = line.strip().split('\t')
+	hour_ip, count = line
+	hour, ip = hour_ip.split(' ')
+
+	try:
+		hour = int(hour)
+	    count = int(count)
+	    top_ip[hour].append([ip, count])
+
+	except ValueError:
+		pass
+
+for index in range(24):
+    top_ip_three = sorted(top_ip[index], key=lambda v:v[1], reverse=True)[0:3]
+    print '%s\t%s' % (index, top_ip_three)
--- mapreduce-test-python/logstat3/test.sh
+++ mapreduce-test-python/logstat3/test.sh
@@ -1,3 +1,25 @@
+
+../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /logstat2/output/part-00000 /logstat3/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-D mapred.reduce.tasks=0 \
+-input /logstat3/input/* -output /logstat3/output/ \
+-file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py \
+# -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py
+/usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+../../stop.sh
+
+
+
+
+
+
 # #!/bin/sh
 # ../../start.sh
 # /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
@@ -23,21 +45,21 @@
 
 
 
-#!/bin/sh
-../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/output/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat3/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--Dmapred.reduce.tasks=1 -Dstream.num.map.output.key.fields=2 -input /logstat3/input/* -output /logstat3/output/count_by_hour.txt -file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py
+# #!/bin/sh
+# ../../start.sh
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+# /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
+# /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/output/
+# /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat3/input/
 # /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
-# -Dmapred.reduce.tasks=1 -Dstream.num.map.output.key.fields=2 -input /logstat3/output/count_by_hour -output /logstat3/output/top_count_by_hour -file ../../mapreduce-test-python/logstat3/mapper2.py -mapper ../../mapreduce-test-python/logstat3/mapper2.py -file ../../mapreduce-test-python/logstat3/reducer2.py -reducer ../../mapreduce-test-python/logstat3/reducer2.py
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/count_by_hour.txt
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
-../../stop.sh
+# -Dmapred.reduce.tasks=1 -Dstream.num.map.output.key.fields=2 -input /logstat3/input/* -output /logstat3/output/count_by_hour.txt -file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py
+# # /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+# # -Dmapred.reduce.tasks=1 -Dstream.num.map.output.key.fields=2 -input /logstat3/output/count_by_hour -output /logstat3/output/top_count_by_hour -file ../../mapreduce-test-python/logstat3/mapper2.py -mapper ../../mapreduce-test-python/logstat3/mapper2.py -file ../../mapreduce-test-python/logstat3/reducer2.py -reducer ../../mapreduce-test-python/logstat3/reducer2.py
+# /usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/count_by_hour.txt
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
+# /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
+# ../../stop.sh
 
 
 # /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
