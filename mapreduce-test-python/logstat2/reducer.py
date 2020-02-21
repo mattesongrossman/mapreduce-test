675ff8a8255fe74bb448563c78437c7c3ce046ca
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-21 12:44:11 -0500

    only 1 mapreduce-3

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -34,9 +34,9 @@ for line in sys.stdin:
 
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 for ip, count in sorted_dict_ip_count:
-    # print '%s\t%s' % (ip, count)
-    hour_ip  = ip.strip().split('\t')
-    hour = hour_ip[1:3]
+    print '%s\t%s' % (ip, count)
+    # hour_ip  = ip.strip().split('\t')
+    hour = ip[1:3]
     ip = hour_ip[7:]
     hour = int(hour)
     count = int(count)
@@ -45,13 +45,13 @@ for ip, count in sorted_dict_ip_count:
     else:
         top_ip[hour].append([ip, count])
 
-for index in range(24):
-    if index not in top_ip.keys():
-        # print("no key = " + str(index))
+for hour in range(24):
+    if hour not in top_ip.keys():
+        print("no key = " + str(hour))
         pass
     else:
-        top_ip_three = sorted(top_ip[index], key=itemgetter(1), reverse=True)[0:3]
-        # print (index, top_ip_three)
-
-for hour, ip, count in top_ip_three:
-    print '%s\t%s' % (hour, ip, count)
+        top_ip_three = sorted(top_ip[hour], key=itemgetter(1), reverse=True)[0:3]
+        print (hour, top_ip_three)
+#
+# for hour, ip, count in top_ip_three:
+#     print '%s\t%s' % (hour, ip, count)
--- mapreduce-test-python/logstat3/mapper.py
+++ mapreduce-test-python/logstat3/mapper.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python
-
-from operator import itemgetter
+# --*-- coding:utf-8 --*--
+import re
 import sys
 
-# file_object = open("log-format.txt","r")
-
-top_ip = {}
-
-
-# print(file_object.readline())
-
 for line in sys.stdin:
-    # print(line)
-    line = line.strip().split('\t')
-    hour_ip, count = line
-    hour = hour_ip[1:3]
-    ip = hour_ip[7:]
-    hour = int(hour)
-    count = int(count)
-    if hour not in top_ip.keys():
-        top_ip[hour] = [[ip, count]]
-    else:
-        top_ip[hour].append([ip, count])
-
-
-for index in range(24):
-    if index not in top_ip.keys():
-        print("no key = " + str(index))
-    else:
-        top_ip_three = sorted(top_ip[index], key=itemgetter(1), reverse=True)[0:3]
-        print (index, top_ip_three)
+	print '%s\t%s' % (line, 1)
