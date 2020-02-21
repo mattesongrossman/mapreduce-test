26263735c0a48adebb1ccba09cdc7d5815afa67e
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-21 12:50:07 -0500

    2 mapreduces-1

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -34,24 +34,24 @@ for line in sys.stdin:
 
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 for ip, count in sorted_dict_ip_count:
-    # print '%s\t%s' % (ip, count)
+    print '%s\t%s' % (ip, count)
     # hour_ip  = ip.strip().split('\t')
-    hour = ip[1:3]
-    ip = hour_ip[7:]
-    hour = int(hour)
-    count = int(count)
-    if hour not in top_ip.keys():
-        top_ip[hour] = [[ip, count]]
-    else:
-        top_ip[hour].append([ip, count])
-
-for hour in range(24):
-    if hour not in top_ip.keys():
-        print("no key = " + str(hour))
-        pass
-    else:
-        top_ip_three = sorted(top_ip[hour], key=itemgetter(1), reverse=True)[0:3]
-        print (hour, top_ip_three)
+#     hour = ip[1:3]
+#     ip = hour_ip[7:]
+#     hour = int(hour)
+#     count = int(count)
+#     if hour not in top_ip.keys():
+#         top_ip[hour] = [[ip, count]]
+#     else:
+#         top_ip[hour].append([ip, count])
+#
+# for hour in range(24):
+#     if hour not in top_ip.keys():
+#         print("no key = " + str(hour))
+#         pass
+#     else:
+#         top_ip_three = sorted(top_ip[hour], key=itemgetter(1), reverse=True)[0:3]
+#         print (hour, top_ip_three)
 #
 # for hour, ip, count in top_ip_three:
 #     print '%s\t%s' % (hour, ip, count)
