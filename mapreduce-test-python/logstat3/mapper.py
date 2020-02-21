bb3a9207bc5c3dd743ce509e09e4240d1c27855a
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2020-02-21 12:59:10 -0500

    ip hour test

--- mapreduce-test-python/logstat3/mapper.py
+++ mapreduce-test-python/logstat3/mapper.py
@@ -3,5 +3,9 @@
 import re
 import sys
 
+pat = re.compile('(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}:\d{2}).*? ')
+
 for line in sys.stdin:
-	print '%s\t%s' % (line, 1)
+    match = pat.search(line)
+    if match:
+        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
