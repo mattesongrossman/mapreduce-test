cd0f8a9a3800a907ea023cb124568539f3594dab
Matteson Grossman <mattesongrossman@mattesons-mbp.home>
2020-03-18 11:10:18 -0400

    change path in test.sh

--- mapreduce-test-python/PV/test.sh
+++ mapreduce-test-python/PV/test.sh
@@ -8,8 +8,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /PV/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/PV_Data.csv* /PV/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file /project1/part1/PV/mapper.py -mapper /mapreduce-test/mapreduce-test-python/PV/mapper.py \
--file /project1/part1/PV/reducer.py -reducer /mapreduce-test/mapreduce-test-python/PV/reducer.py \
+-file /mapreduce-test/mapreduce-test-python/PV/mapper.py -mapper /mapreduce-test/mapreduce-test-python/PV/mapper.py \
+-file /mapreduce-test/mapreduce-test-python/PV/reducer.py -reducer /mapreduce-test/mapreduce-test-python/PV/reducer.py \
 -input /PV/input/* -output /PV/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /PV/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /PV/input/
